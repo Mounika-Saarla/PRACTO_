9b4050896efe956b7103f6aa6fdf5a04c4b949a5
SripathiArchana <SRIPATHI-ARCHANA.SRIPATHI-ARCHANA@CAPGEMINI.COM>
2025-11-12 20:35:09 +0530

    Search_for_Clinics_code second commit

--- Practo/src/test/java/com/parameters/ExcelReader.java
+++ Practo/src/test/java/com/parameters/ExcelReader.java
@@ -1,30 +1,73 @@
-
-
-
 package com.parameters;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import com.pages.userpage;
+
+import java.io.FileInputStream;
+
 public class ExcelReader {
-    private static String filePath;
-
-    public ExcelReader(String filePath) {
-        this.filePath = filePath;
+    public String getCellData(int row, int col) throws Exception {
+    	String excelPath = "C:\\Training\\SprintPracto\\Practo\\src\\test\\resources\\TestData\\Practo_Automation.xlsx";
+        FileInputStream fis = new FileInputStream(excelPath);
+        Workbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheetAt(0);
+        String data = sheet.getRow(row).getCell(col).getStringCellValue();
+        workbook.close();
+        return data;
     }
 
-    public static String getSpecializationFromExcel() throws IOException {
-        try (FileInputStream fis = new FileInputStream(filePath);
-             XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
+    public String getLocation(int row) throws Exception {
+        return getCellData(row, 0); // Column 0 for Location
+    }
 
-            Sheet sheet = workbook.getSheetAt(0); // Sheet1 = index 0
-            Row row = sheet.getRow(0);            // Row 1 = index 0
-            Cell cell = row.getCell(0);           // Column A = index 0
+    public String getSpeciality(int row) throws Exception {
+        return getCellData(row, 1); // Column 1 for Speciality
+    }
+    
 
-            DataFormatter formatter = new DataFormatter();
-            return formatter.formatCellValue(cell);
+
+public static String getCellData(String sheetName, int row, int col) throws Exception {
+        String excelPath = PropertyReader.getProperty("excelFilePath");
+        FileInputStream fis = new FileInputStream(excelPath);
+        Workbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheet(sheetName);
+
+        if (sheet == null) {
+            workbook.close();
+            throw new RuntimeException("Sheet '" + sheetName + "' not found in Excel file.");
         }
+
+        Row r = sheet.getRow(row);
+        if (r == null) {
+            workbook.close();
+            throw new RuntimeException("Row " + row + " not found in sheet '" + sheetName + "'");
+        }
+
+        Cell c = r.getCell(col);
+        if (c == null) {
+            workbook.close();
+            throw new RuntimeException("Cell [" + row + "," + col + "] not found in sheet '" + sheetName + "'");
+        }
+
+        String value;
+        if (c.getCellType() == CellType.NUMERIC) {
+            value = String.valueOf((int) c.getNumericCellValue());
+        } else {
+            value = c.getStringCellValue();
+        }
+
+        workbook.close();
+        return value;
     }
+public String getCity(int row) throws Exception {
+    return getCellData(row, 0); // Column 0 for city
+}
+
+public String getRole(int row) throws Exception {
+    return getCellData(row, 1); // Column 1 for Role
+}
+
+
 }
--- Practo/src/test/java/com/setup/BaseSteps.java
+++ Practo/src/test/java/com/setup/BaseSteps.java
@@ -1,121 +1,56 @@
-//package com.setup;
-// 
-//import java.util.Properties;
-// 
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.edge.EdgeDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-// 
-//import com.parameters.PropertyReader;
-// 
-//public class BaseSteps {
-// 
-//	public static WebDriver driver;
-//	
-//	public static void launchBrowser() {
-//        Properties prop = PropertyReader.readProperty();
-//        String browser = prop.getProperty("browserName"); //chrome will open
-//        
-// 
-//        if (browser.equalsIgnoreCase("chrome")) {
-//            
-//            System.setProperty("webdriver.chrome.driver", "C:\\Training\\SeleniumGrid\\chromedriver.exe");
-//            driver = new ChromeDriver();
-//        }
-//        else if (browser.equalsIgnoreCase("firefox")) {
-//            driver = new FirefoxDriver();
-//        }
-//        else if (browser.equalsIgnoreCase("edge"))
-//        {
-//         	//System.setProperty("webdriver.edge.driver", "C:\\Training\\SeleniumGrid\\msedgedriver.exe");
-//            driver = new EdgeDriver();
-//        }
-//        else {
-//            System.out.println("Invalid browser specified in config.properties");
-//            return;
-//        }
-//        String url = prop.getProperty("sourceUrl");
-//        driver.get(url);
-//        //maximizw the screen
-//        driver.manage().window().maximize();
-//       
-//    }
-// 
-//    public static void sleep(int msec) { //hardcoded implementation, handling waits
-//        try
-//        {
-//            Thread.sleep(2000);
-//        }
-//        catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
-// 
-// 
-
-
-
 package com.setup;
 
+import java.time.Duration;
 import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 import com.parameters.PropertyReader;
 
-public class BaseSteps {
+import io.github.bonigarcia.wdm.WebDriverManager;
 
+public class BaseSteps {
     public static WebDriver driver;
+    public static Properties prop;
 
     public static void launchBrowser() {
-        Properties prop = PropertyReader.readProperty();
+        prop = PropertyReader.readProperty();
+        String browser = prop.getProperty("browser");
+        String appUrl = prop.getProperty("appUrl");
 
-        if (prop == null) {
-            throw new RuntimeException("Failed to load config.properties. PropertyReader returned null.");
-        }
-
-        String browser = prop.getProperty("browserName");
-        String url = prop.getProperty("sourceUrl");
-
-        if (browser == null || url == null) {
-            throw new RuntimeException("Missing browserName or sourceUrl in config.properties");
-        }
-
-        switch (browser.toLowerCase()) {
-            case "chrome":
-                System.setProperty("webdriver.chrome.driver", "C:\\Training\\SeleniumGrid\\chromedriver.exe");
-                driver = new ChromeDriver();
-                break;
-            case "firefox":
-                System.setProperty("webdriver.gecko.driver", "C:\\Training\\SeleniumGrid\\geckodriver.exe");
-                driver = new FirefoxDriver();
-                break;
-            case "edge":
-                System.setProperty("webdriver.edge.driver", "C:\\Training\\SeleniumGrid\\msedgedriver.exe");
-                driver = new EdgeDriver();
-                break;
-            default:
-                throw new RuntimeException("Invalid browser specified: " + browser);
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else {
+            throw new RuntimeException("Invalid browser specified: " + browser);
         }
 
         driver.manage().window().maximize();
-        driver.get(url);
+        driver.get(appUrl); // Navigate here
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+    }
+    public static void navigateToSearchClinicUrl() {
+        String searchClinicUrl = prop.getProperty("SearchClinicUrl");
+        if (searchClinicUrl == null) {
+            throw new RuntimeException("SearchClinicUrl is missing in profile.properties");
+        }
+        driver.get(searchClinicUrl);
     }
 
-    public static void sleep(int msec) {
-        try {
-            Thread.sleep(msec);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public static WebDriver getDriver() {
+        return driver;
     }
 
     public static void closeBrowser() {
         if (driver != null) {
             driver.quit();
+            driver = null;
         }
     }
+
 }
+    
+
+
+   
